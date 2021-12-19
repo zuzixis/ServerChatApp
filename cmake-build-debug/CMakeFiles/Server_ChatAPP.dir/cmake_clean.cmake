7b42b8f7be50aee731d78fa5f9873f689c2f8e7e
Zuzana Žillová <zuzka.zillova@gmail.com>
2021-12-19 21:29:15 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/Server_ChatAPP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Server_ChatAPP.dir/main.cpp.o"
+  "Server_ChatAPP.pdb"
+  "Server_ChatAPP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Server_ChatAPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
