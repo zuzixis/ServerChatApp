4fa9f98d9beb17e252c7c9dcd30d9073af60f3c8
roncakjakub <51802574+roncakjakub@users.noreply.github.com>
2021-12-27 21:13:22 +0100

    Pridane vlakno do mainu

---
+++ providers/ActiveUsersProvider.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Jakub Rončák on 26/12/2021.
+//
+
+#include "ActiveUsersProvider.h"
+
+void ActiveUsersProvider::addUser(User *u) {
+    this->activeUsers->push_back(u);
+}
+
+void ActiveUsersProvider::removeUser(const User *u) {
+
+    remove(this->activeUsers->begin(), this->activeUsers->end(), u);
+    //TODO: skontrolovat
+}
+
+const vector<User *> *ActiveUsersProvider::getActiveUsers() {
+    return activeUsers;
+}
+
+User *ActiveUsersProvider::getLastUser() {
+    return activeUsers->back();
+}
