b29a11d6f6063496159bb640c6f95296af663ac3
roncakjakub <51802574+roncakjakub@users.noreply.github.com>
2022-01-05 22:46:01 +0100

    Pri vyhladavani kontaktov som vylucil prihlaseneho usera

--- controllers/UserController.cpp
+++ controllers/UserController.cpp
@@ -14,44 +14,28 @@ UserController::~UserController() {
 
 string UserController::search(const json *data) {
     cout << *data << endl;
-//    int userId = ActiveUsersProvider::getInstance().getActualUserId();
+    int userId = ActiveUsersProvider::getInstance().getActualUserId();
     // {or:{x:1,y:2}}
 
     if (!data->contains("word")) {
-        return R"({"status": 422,"data":{}})";
+        return R"({"status": 422,"data":{"errors":[{"word":"Attribute is required"}]}})";
     }
 
-//    string filtersString = "{\"user_id\":" + to_string(userId);// +"}";
-//    if (data->contains("word")) {
-        string x = data->at("word");
+    string x = data->at("word");
     string filtersString = R"({"name":"LIKE:)" + x + "\"}";
-//    }
-//    filtersString += "}";
 
     json filters = json::parse(filtersString);
 
     json loadedUsers;
     JsonReader::read("database/users.json", filters, loadedUsers);
 
-    // {"or":[{"id":1},{"id":2}]}
+    json ret;
+    copy_if(
+            loadedUsers.begin(), loadedUsers.end(),
+            back_inserter(ret), [&userId](const json &item) {
+                return (int) (item["id"]) == userId;
+            });
 
-//    string groupFiltersString = "{\"or\":[";
-//    int x;
-//    for (auto loadedGroupId: loadedGroupIds) {
-//        x = loadedGroupId["group_id"];
-//        groupFiltersString += ("{\"id\":" + to_string(x) + "},");
-//    }
-//    groupFiltersString = groupFiltersString.substr(0, groupFiltersString.size() - 1);
-//    groupFiltersString += "]}";
-//
-//    json groupFilters = json::parse(groupFiltersString);
-//
-//    json loadedGroups;
-//    JsonReader::read("database/groups.json", groupFilters, loadedGroups);
-//    ActiveUsersProvider activeUsersProvider = ActiveUsersProvider::getInstance();
-// TODO: ideme davat do usera spravy?
-
-//    return user->getMessages();
-    cout << loadedUsers << endl;
-    return R"({"status": 200,"data":)" + (!loadedUsers.empty() ? loadedUsers.dump() : "[]") + "}";
+    cout << ret << endl;
+    return R"({"status": 200,"data":)" + (!ret.empty() ? ret.dump() : "[]") + "}";
 }
